6aaf35eb88463b1c144f259c92f855c36ad3c0ce
unSerori <pd.ai.374005475@gmail.com>
2024-04-20 09:51:36 +0000

    [add]mysqlに接続[要検討]

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.2
 require github.com/gin-gonic/gin v1.9.1
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/bytedance/sonic v1.11.5 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
@@ -15,10 +16,12 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.19.0 // indirect
-	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
--- main.go
+++ main.go
@@ -3,18 +3,58 @@ package main
 
 // imports
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
 )
+
+// TODO: モデルの定義
+type Users struct { // typeで型の定義, structは構造体
+	ID   uint   `gorm:"primary_key5"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
 
 // main method
 func main() {
+	// TODO: log func
+
 	// エンジンを作成
 	engine := gin.Default()
 
 	// SQL接続
+	// .envから定数をプロセスの環境変数にロード
+	err := godotenv.Load(".env") // エラーを格納
+	if err != nil {              // エラーがあったら
+		panic("Error loding .env file")
+	}
+	// 環境変数から取得
+	dbUser := os.Getenv("MYSQL_USER")
+	dbPass := os.Getenv("MYSQL_PASSWORD")
+	dbHost := os.Getenv("MYSQL_HOST")
+	//dbPort := os.Getenv("MYSQL_PORT")
+	dbDB := os.Getenv("MYSQL_DATABASE")
+	// 接続
+	// Mysqlに接続
+	db, err := gorm.Open( // dbとエラーを取得
+		"mysql", // dbの種類
+		fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbDB), // 接続情報
+		// fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbPort, dbDB), // 接続情報
+	)
+	if err != nil { // エラー処理
+		fmt.Println("せつぞくできなかった")
+		log.Fatal("Couldnt connect to the db server.", err)
+	}
+	defer db.Close() // defer文でこの関数が終了した際に破棄する
+
+	db.AutoMigrate(&Users{})
 
 	// テンプレートと静的ファイルを読み込む
 	engine.LoadHTMLGlob("views/*.html")
