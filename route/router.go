c02daa8c427e400037c689edb6ce4047a88c8618
unSerori <pd.ai.374005475@gmail.com>
2024-04-24 04:43:24 +0000

    [fix]変更が反映されない問題-2

--- route/router.go
+++ route/router.go
@@ -1,7 +1,7 @@
 package route
 
 import (
-	//"Kakuninkun_server/controller"
+	"Kakuninkun_server/controller"
 	"Kakuninkun_server/middleware"
 
 	"github.com/gin-gonic/gin"
@@ -15,9 +15,9 @@ func GetRouter() *gin.Engine {
 	// mid all
 	engine.Use(middleware.MidLog)
 	// /
-	// engine.GET("/", controller.ShowRootPage)
-	// // /json
-	// engine.GET("/json", controller.ShowTPage)
+	engine.GET("/", controller.ShowRootPage)
+	// /json
+	engine.GET("/json", controller.ShowTPage)
 
 	return engine // router設定されたengineを返す。
 }
